60761046c2fe8d969a13ac3a6e756dc1e92537a8
txd825 <txd825@gmail.com>
2016-05-04 10:22:53 +0800

    调整枪管的方向

---
+++ F-Game/Assets/FreeMachineGun/Scripts/FireTheGun.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTheGun : MonoBehaviour {
+
+    private GameObject MyGun;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
