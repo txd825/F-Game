a51ff268db99d72ed3c2c1d3450c184e0fe04f7a
txd825 <txd825@gmail.com>
2016-05-17 13:25:46 +0800

    通过点击鼠标控制特效播放和消失

---
+++ F-Game/Assets/Script/GunFire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunFire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if(Time.frameCount%5 == 0)
+            {
+                print("fire");
+               
+            }
+        }
+	
+	}
+
+    
+}
---
+++ F-Game/Assets/Script/PlayFirePartical.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayFirePartical : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+       /** if (Input.GetKey(KeyCode.Mouse0))
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+            gameObject.GetComponent<ParticleSystem>().Pause();
+            gameObject.GetComponent<ParticleSystem>().Stop();
+        }**/
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            //gameObject.GetComponent<ParticleSystem>().Pause();
+            gameObject.GetComponent<ParticleSystem>().Stop();
+        }
+
+    }
+}
